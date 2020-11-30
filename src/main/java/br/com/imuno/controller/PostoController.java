3a2582e9b8c661c2fa5fbd75ffa421e86462a744
Mateus Vilione <mateusvilione@gmail.com>
2020-11-30 18:03:08 -0300

    Ajuste posto

--- src/main/java/br/com/imuno/controller/PostoController.java
+++ src/main/java/br/com/imuno/controller/PostoController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.imuno.dto.PostoDTO;
-import br.com.imuno.model.Paciente;
 import br.com.imuno.model.Posto;
 import br.com.imuno.request.PostoRequest;
 import br.com.imuno.service.PostoService;
